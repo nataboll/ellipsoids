0be8764a5d0a4841d235b342e08126b2c9ff674e
natasha <nataboll@mail.ru>
2018-05-02 20:02:55 +0300

    change data.py

--- src/data.py
+++ src/data.py
@@ -14,12 +14,12 @@ class Data:
     x = np.zeros((n, m))    # n strings of m indexes: x and y
 
 
-def generate(self):
-    self.x = 10 * np.random.randn(self.n, self.m)
-    # add a few outliers to the set of points:
-    for i in range(2):
-        self.x[i, 50] = 100 * np.random.randn()
-        self.x[i, 60] = 100 * np.random.randn()
-        self.x[i, 80] = 100 * np.random.randn()
-        self.x[i, 40] = 100 * np.random.randn()
-    return self.x
+    def generate(self):
+        self.x = 10 * np.random.randn(self.n, self.m)
+        # add a few outliers to the set of points:
+        for i in range(2):
+            self.x[i, 50] = 100 * np.random.randn()
+            self.x[i, 60] = 100 * np.random.randn()
+            self.x[i, 80] = 100 * np.random.randn()
+            self.x[i, 40] = 100 * np.random.randn()
+        return self.x
