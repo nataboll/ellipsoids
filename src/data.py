2fac386bf9041df7be7bda0d3091ffa3d2789562
Suerte412 <38894413+Suerte412@users.noreply.github.com>
2018-05-06 18:24:37 +0300

    Naive value calculation for each point

--- src/data.py
+++ src/data.py
@@ -11,14 +11,17 @@ class Data:
     def __init__(self):
         pass
 
-    n = 2
-    m = 100
+    k = 0.05  # coefficient for the closest neighbours
+    n = 2  # number of rows
+    m = 100   # number of columns
     x = np.zeros((n, m))    # n strings of m indexes: x and y
-    df = pd.DataFrame(data=x)
+    values = np.zeros(m)  # array for density parameter of a point
+    df = pd.DataFrame(data=x)  # empty DataFrame
 
+    # generating data
     def generate(self):
         self.x = 10 * np.random.randn(self.n, self.m)
-        # add a few outliers to the set of points:
+        # adding a few outliers to the set of points:
         for i in range(2):
             self.x[i, 50] = 100 * np.random.randn()
             self.x[i, 60] = 100 * np.random.randn()
@@ -26,7 +29,22 @@ class Data:
             self.x[i, 40] = 100 * np.random.randn()
             self.x[i, 1] = 100 * np.random.randn()
 
+        # creating an array of distances between each two points
+        dist = np.zeros((self.m, self.m))
+        # filling it with 2nd norm distances
+        for j in range(self.m):
+            for k in range(j, self.m):  # filling upper triangle of the symmetric matrix 'dist'
+                dist[j, k] = np.linalg.norm(self.x[:, j] - self.x[:, k])
+                if j != k:
+                    dist[k, j] = dist[j, k]  # using symmetry of distances, i.e. dist should be symmetric
+        # for each point counting sum of distances from the k*m nearest neighbours
+        for j in range(self.m):
+            distSort = sorted(dist[j, :])
+            self.values[j] = np.sum([distSort[1:int(np.ceil(self.k * self.m)+1)]])
+
+    # creating DataFrame with coordinates and value
     def toDataFrame(self):
-        # Adding empty row for 'Value'
-        newData = np.append(self.x, np.zeros((1, self.m)), axis=0)
+        # adding empty row for 'value'
+        newData = np.append(self.x, [self.values], axis=0)
+        # creating DataFrame itself
         self.df = pd.DataFrame(data=newData, index=['X', 'Y', 'Value'])
--- src/run.py
+++ src/run.py
@@ -1,15 +1,19 @@
-import pandas as pd
-import numpy as np
-
+from datetime import datetime
 
 from src.data import Data
 
+import pandas as pd
+
+startTime = datetime.now()
 data = Data()
 data.generate()
 data.toDataFrame()
 
-#Display 'a' rows and 'b' columns of dataframe
+
+# display 'a' rows and 'b' columns of dataframe
 a = len(data.df.index)
 b = len(data.df.columns)
 with pd.option_context('display.max_rows', a, 'display.max_columns', b):
     print(data.df)
+
+print("\nExecution time: ", datetime.now() - startTime)
