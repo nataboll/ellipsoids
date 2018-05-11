349ff748bada14e7673e46cc8c0337bdda6dd78a
natasha <nataboll@mail.ru>
2018-05-11 23:17:56 +0300

    testing the way of building function q(...)

--- test/test1.py
+++ test/test1.py
@@ -1,1 +1,12 @@
-# code
+# testing if q(...) was built right
+
+import numpy as np
+
+x = np.array(([1, 1, 1], [3, 4, 5]))
+
+
+def f(a, z, y):
+    return a + z - y
+
+
+print(np.sum(f(6, x[0, :], x[1, :])))
